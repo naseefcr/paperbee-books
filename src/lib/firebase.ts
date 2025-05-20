ede69c8bece155c1ef6012e133b2f59281aa1eb6
naseefcr <99385968+naseefcr@users.noreply.github.com>
2025-05-20 23:31:25 +0530

    Connected to the firebase project

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getAuth } from 'firebase/auth'; // For admin authentication later
 // import { getStorage } from 'firebase/storage'; // If you plan to use Firebase Storage for images
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID, // Optional
+  apiKey: "AIzaSyCMNUIrANqfwR1ro8KAomvn2JDfOrxw4Yw",
+  authDomain: "paperbee-books.firebaseapp.com",
+  projectId: "paperbee-books",
+  storageBucket: "paperbee-books.firebasestorage.app",
+  messagingSenderId: "1092290793122",
+  appId: "1:1092290793122:web:0d5f81668e0451347ad1fb",
+  measurementId: "G-CDZXY3NLLX"
 };
 
 // Initialize Firebase
